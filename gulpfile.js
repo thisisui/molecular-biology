1d77e53b10b049ededa8d4740a017e9ad1cecbe5
Igor Napierala <igornapierala@gmail.com>
2015-06-04 11:53:10 +0200

    test configuration to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,21 @@ var jshint = require('gulp-jshint');
 var nodeunit = require('gulp-nodeunit');
 var plumber = require('gulp-plumber');
 
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function () {
+    gulp.watch('./apps/cut/**/*.js', ['jslint', 'jstest']);
+    gulp.watch('./spec/**/*.js', ['jstest']);
+});
+
+gulp.task('jstest', function () {
+    gulp.src('./spec/**/*.js')
+        .pipe(plumber())
+        .pipe(nodeunit());
+});
+
 gulp.task('jslint', function () {
     return gulp.src('./apps/cut/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-});
-
-gulp.task('watch', function () {
-    gulp.watch('./apps/cut/**/*.js', ['jslint', 'test']);
-    gulp.watch('./spec/**/*.js', ['test']);
-});
-
-gulp.task('test', function () {
-    gulp.src('./spec/**/*.js')
-        .pipe(plumber())
-        .pipe(nodeunit());
 });
--- spec/cut.spec/modules.spec/rulesChecker.spec.js
+++ spec/cut.spec/modules.spec/rulesChecker.spec.js
@@ -7,8 +7,9 @@ exports.prepareSimple = function (test) {
 };
 
 exports.prepareWithOneSpecific = function (test) {
-    test.expect(1);
+    test.expect(2);
     test.deepEqual(rulesChecker.prepare('AGCCGB'), ['AGCCGC', 'AGCCGG', 'AGCCGT']);
+    test.equal(rulesChecker.prepare('AGCCGB').length, 3);
     test.done();
 };
 
