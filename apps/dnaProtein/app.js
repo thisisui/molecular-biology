216cb18db6028ab7339a98695380a5caef91c246
Igor Napierala <igor.napierala@cognifide.com>
2016-02-27 16:02:01 +0100

    Restructuring folders

--- apps/dnaProtein/app.js
+++ apps/dnaProtein/app.js
@@ -2,7 +2,7 @@ var Datastore = require('nedb')
     , db = new Datastore({ filename: 'liveLogger', autoload: true })
     , dnaCreator = require('./partials/dna.js')
     , translation = require('./partials/translation.js')
-    , transcription = require('./partials/transcription.js');  
+    , transcription = require('./partials/transcription.js');
 
 (function createProtein() {
     var protein = translation(transcription(dnaCreator()));
